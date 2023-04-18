b9dc75ec2c8b0428873002c275471b3658ea558a
Samyr <samirgamesmarjoub@gmail.com>
2023-04-18 19:46:11 -0300

    ..

---
+++ pages/api/setStatusTicket.ts
@@ -0,0 +1,14 @@
+import knex from './db'
+
+export default async function updateMsg(req, res) {
+  const id = req.query.id
+  try {
+    const result = await knex.raw(
+      `UPDATE msgs SET msg = ?, titulo = ? WHERE id = ?`,
+      [req.body.msg, req.body.titulo, id]
+    ); res.json(result[0]);
+    // console.log(result[0])
+  } catch (error) {
+    res.send(error);
+  }
+}
